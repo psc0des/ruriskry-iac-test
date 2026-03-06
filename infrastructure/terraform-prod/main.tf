0a12d459e557828aebf433fc245decef4a41de2e
psc0des <sarathy.vass6@gmail.com>
2026-03-06 16:13:46 +0530

    Change SSH access from Deny to Allow

--- infrastructure/terraform-prod/main.tf
+++ infrastructure/terraform-prod/main.tf
@@ -184,7 +184,7 @@ resource "azurerm_network_security_group" "prod" {
     name                       = "allow-ssh-anywhere"
     priority                   = 140
     direction                  = "Inbound"
-    access                     = "Deny"
+    access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
